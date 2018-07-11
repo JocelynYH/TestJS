53fcb9e00dd1f29e6dad98d1ac3e69fb03af96b2
Jimmy <seawee65@live.com>
2018-07-11 19:11:32 -0400

    fixed init database

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ var http = require ('http');
 
 var mysql = require ('mysql');
 
-var pool = mysql.createPool ({ process.env.DB_HOST, process.env.DB_USER, process.env.DB_PASSWORD, process.env.DB_DATABASE });
+var pool = mysql.createPool ({
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_DATABASE
+});
 
 http.createServer (
 
