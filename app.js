e57d1910846a34cb63b09e6922ea4cb0311c8436
Jimmy <seawee65@live.com>
2018-07-11 19:07:51 -0400

    test mysql

--- app.js
+++ app.js
@@ -2,11 +2,27 @@
 
 var http = require ('http');
 
+var mysql = require ('mysql');
+
+var pool = mysql.createPool ({ process.env.DB_HOST, process.env.DB_USER, process.env.DB_PASSWORD, process.env.DB_DATABASE });
+
 http.createServer (
+
 	(req, res) => {
 
 		res.writeHead (200);
-		res.end ('Hello World 11');
+
+		var abc = pool.query('select body from messages',
+
+			(error, results, fields) => {
+
+				if (error) { throw error; res.end ('there was a SQL error.'); }
+
+				res.end (results[0]);
+
+			}
+
+		);
 
 	}
 ).listen (8080);
